d9acd849b3aac2db32f523c7d9f31c678be6abd7
nfphys <nfphys@gmail.com>
2022-08-14 11:51:11 +0900

    add test code for UnionFind#size

--- union_find/test/union_find_test.rb
+++ union_find/test/union_find_test.rb
@@ -6,24 +6,59 @@ class UnionFindTest < Minitest::Test
   def test_union_find
     uf = UnionFind.new(7)
     assert_equal "{0}, {1}, {2}, {3}, {4}, {5}, {6}", uf.to_s
+    assert_equal 1, uf.size(0)
+    assert_equal 1, uf.size(1)
+    assert_equal 1, uf.size(2)
+    assert_equal 1, uf.size(3)
+    assert_equal 1, uf.size(4)
+    assert_equal 1, uf.size(5)
+    assert_equal 1, uf.size(6)
 
     uf.unite(1, 2)
     assert uf.same?(1, 2)
     assert_equal "{0}, {1, 2}, {3}, {4}, {5}, {6}", uf.to_s
+    assert_equal 1, uf.size(0)
+    assert_equal 2, uf.size(1)
+    assert_equal 2, uf.size(2)
+    assert_equal 1, uf.size(3)
+    assert_equal 1, uf.size(4)
+    assert_equal 1, uf.size(5)
+    assert_equal 1, uf.size(6)
 
     uf.unite(2, 3)
     assert uf.same?(1, 2)
     assert uf.same?(1, 3)
     assert uf.same?(2, 3)
     assert_equal "{0}, {1, 2, 3}, {4}, {5}, {6}", uf.to_s
+    assert_equal 1, uf.size(0)
+    assert_equal 3, uf.size(1)
+    assert_equal 3, uf.size(2)
+    assert_equal 3, uf.size(3)
+    assert_equal 1, uf.size(4)
+    assert_equal 1, uf.size(5)
+    assert_equal 1, uf.size(6)
 
     uf.unite(5, 6)
     assert uf.same?(5, 6)
     refute uf.same?(1, 6)
     assert_equal "{0}, {1, 2, 3}, {4}, {5, 6}", uf.to_s 
+    assert_equal 1, uf.size(0)
+    assert_equal 3, uf.size(1)
+    assert_equal 3, uf.size(2)
+    assert_equal 3, uf.size(3)
+    assert_equal 1, uf.size(4)
+    assert_equal 2, uf.size(5)
+    assert_equal 2, uf.size(6)
 
     uf.unite(1, 6)
     assert uf.same?(1, 6)
     assert_equal "{0}, {1, 2, 3, 5, 6}, {4}", uf.to_s
+    assert_equal 1, uf.size(0)
+    assert_equal 5, uf.size(1)
+    assert_equal 5, uf.size(2)
+    assert_equal 5, uf.size(3)
+    assert_equal 1, uf.size(4)
+    assert_equal 5, uf.size(5)
+    assert_equal 5, uf.size(6)
   end
 end
