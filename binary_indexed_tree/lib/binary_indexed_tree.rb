556e6646ae9e9f4442ee9b0dd955967bd82d62dc
nfphys <nfphys@gmail.com>
2022-08-14 14:59:32 +0900

    implement add and sum

--- binary_indexed_tree/lib/binary_indexed_tree.rb
+++ binary_indexed_tree/lib/binary_indexed_tree.rb
@@ -1,2 +1,59 @@
 class BinaryIndexedTree 
+
+  attr_reader :size 
+
+  def initialize(n, v_init=nil)
+    @size = n 
+    @nodes = Array.new(n+1, 0)
+    if v_init 
+      (1..@size).each do |i| 
+        add(i, v_init)
+      end
+    end
+  end
+
+  # v[a] += w 
+  def add(a, w)
+    x = a
+    while x <= size 
+      nodes[x] += w 
+      x += interval_length(x)
+    end
+  end
+
+  # v[1] + ... + v[a]
+  def sum(a)
+    prefix_sum = 0 
+    x = a 
+    while x > 0 
+      prefix_sum += nodes[x]
+      x -= interval_length(x)
+    end
+    prefix_sum 
+  end
+
+  def to_a 
+    prefix_sums = Array.new(size+1, 0)
+    (1..size).each do |a|
+      prefix_sums[a] = sum(a)
+    end
+
+    ary = Array.new(size+1)
+    (1..size).each do |a| 
+      ary[a] = prefix_sums[a] - prefix_sums[a-1]
+    end
+
+    ary[1..size] 
+  end
+
+  private 
+
+  # nodes[x]: x番目のノードの値
+  attr_reader :nodes 
+
+  # x番目のノードが管理する区間の長さ
+  def interval_length(x)
+    x & (-x)
+  end
+
 end
--- binary_indexed_tree/test/binary_indexed_tree_test.rb
+++ binary_indexed_tree/test/binary_indexed_tree_test.rb
@@ -6,4 +6,35 @@ class BinaryIndexedTreeTest < Minitest::Test
   def test_binary_indexed_tree
     assert BinaryIndexedTree
   end
+
+  def test_initialize 
+    bit = BinaryIndexedTree.new(4, 2)
+    assert_equal [2, 2, 2, 2], bit.to_a
+  end
+
+  def test_add
+    bit = BinaryIndexedTree.new(4)
+    bit.add(1, 2)
+    assert_equal [2, 0, 0, 0], bit.to_a
+    bit.add(2, 3)
+    assert_equal [2, 3, 0, 0], bit.to_a
+    bit.add(3, 1)
+    assert_equal [2, 3, 1, 0], bit.to_a 
+    bit.add(4, 5)
+    assert_equal [2, 3, 1, 5], bit.to_a
+  end
+
+  def test_sum 
+    bit = BinaryIndexedTree.new(5, 0)
+    bit.add(2, 5)
+    bit.add(3, 2)
+    bit.add(4, 7)
+    bit.add(5, 1)
+    assert_equal [0, 5, 2, 7, 1], bit.to_a
+    assert_equal 0,  bit.sum(1)
+    assert_equal 5,  bit.sum(2)
+    assert_equal 7,  bit.sum(3)
+    assert_equal 14, bit.sum(4)
+    assert_equal 15, bit.sum(5)
+  end
 end
