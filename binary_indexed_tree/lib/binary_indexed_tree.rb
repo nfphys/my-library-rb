d943ddb3a1f57d176c4f4e2ae3eeb7694887268a
nfphys <nfphys@gmail.com>
2022-08-14 14:09:32 +0900

    create BinaryIndexedTree

---
+++ binary_indexed_tree/lib/binary_indexed_tree.rb
@@ -0,0 +1,2 @@
+class BinaryIndexedTree 
+end
---
+++ binary_indexed_tree/test/binary_indexed_tree_test.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/binary_indexed_tree'
+
+require 'minitest/autorun'
+
+class BinaryIndexedTreeTest < Minitest::Test 
+  def test_binary_indexed_tree
+    assert BinaryIndexedTree
+  end
+end
