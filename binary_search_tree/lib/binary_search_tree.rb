0d91013b6ba6be35db6d2a6611bbcb0a30aed41d
nfphys <nfphys@gmail.com>
2022-08-16 18:20:12 +0900

    rename method 'inorder_successor' to 'successor' and change its visibility to public

--- binary_search_tree/lib/binary_search_tree.rb
+++ binary_search_tree/lib/binary_search_tree.rb
@@ -100,7 +100,7 @@ class BinarySearchTree
     return false if node.nil? 
 
     if node.left && node.right
-      successor = inorder_successor(node)
+      successor = self.successor(node)
       successor_key = successor.key 
       delete(successor_key)
       node.key = successor_key
@@ -216,17 +216,8 @@ class BinarySearchTree
     preorder
   end
 
-  private 
-
-  # to_preorderの補助メソッド
-  def _to_preorder(node, preorder)
-    preorder << node
-    self._to_preorder(node.left,  preorder) if node.left 
-    self._to_preorder(node.right, preorder) if node.right 
-  end
-
   # 通りがけ順でnodeの次のノードを見つける
-  def inorder_successor(node)
+  def successor(node)
     next_node = node.right 
     current_node = nil 
     while next_node 
@@ -238,6 +229,15 @@ class BinarySearchTree
       break  
     end
     current_node
+  end
+
+  private 
+
+  # to_preorderの補助メソッド
+  def _to_preorder(node, preorder)
+    preorder << node
+    self._to_preorder(node.left,  preorder) if node.left 
+    self._to_preorder(node.right, preorder) if node.right 
   end
 
   # rootからnodeへのパス上のノードの高さを更新
