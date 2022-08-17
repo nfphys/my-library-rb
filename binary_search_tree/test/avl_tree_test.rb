1e9529f0b93ca0cf996db6f74b42bf7973c30963
nfphys <nfphys@gmail.com>
2022-08-17 18:11:29 +0900

    add test_insert

--- binary_search_tree/test/avl_tree_test.rb
+++ binary_search_tree/test/avl_tree_test.rb
@@ -7,4 +7,15 @@ class AVLTreeTest < Minitest::Test
     assert AVLTree 
     assert_equal BinarySearchTree, AVLTree.superclass
   end
+
+  def test_insert_case1
+    bst = AVLTree.new([7, 12, 2, 5, 2, 8, 3, 6, 15, 11, 9])
+    assert bst.balanced?
+  end
+
+  def test_insert_case2 
+    bst = AVLTree.new([1, 2, 3, 1, 4, 5, 6])
+    assert bst.balanced?
+  end
+    
 end
