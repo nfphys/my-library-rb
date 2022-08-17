f2fd557c2662d2ff731d06ef2c984988f42c30c3
nfphys <nfphys@gmail.com>
2022-08-17 21:33:59 +0900

    create MultiSet

---
+++ binary_search_tree/lib/multiset.rb
@@ -0,0 +1,5 @@
+require_relative 'avl_tree'
+
+class MultiSet 
+  
+end
---
+++ binary_search_tree/test/multiset_test.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/multiset'
+
+require 'minitest/autorun'
+
+class MultiSetTest < Minitest::Test 
+  def test_multi_set
+    assert MultiSet 
+  end
+end
