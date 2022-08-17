ae86fa6ce5d85d3a7ae2b412ad0a88d8c413e950
nfphys <nfphys@gmail.com>
2022-08-17 21:47:39 +0900

    add 'initialize', 'insert', 'delete', 'count', 'max' and 'min'

--- binary_search_tree/lib/multiset.rb
+++ binary_search_tree/lib/multiset.rb
@@ -1,5 +1,49 @@
 require_relative 'avl_tree'
 
 class MultiSet 
+
+  def initialize(ary=[])
+    @bst = AVLTree.new
+    ary.each do |key|
+      self.insert(key)
+    end 
+  end
+
+  def insert(key)
+    node = bst.find(key)
+    if node 
+      node.value += 1
+    else 
+      bst.insert(key, 1)
+    end
+  end
+
+  def delete(key, count)
+    node = bst.find(key)
+    return if node.nil? 
+
+    if node.value > count 
+      node.value -= count 
+    else 
+      bst.delete(key)
+    end
+  end
+
+  def count(key)
+    node = bst.find(key)
+    return 0 if node.nil? 
+    node.value 
+  end
+
+  def max 
+    bst.max 
+  end
+
+  def min 
+    bst.min 
+  end
   
+  private 
+
+  attr_reader :bst 
 end
--- binary_search_tree/test/multiset_test.rb
+++ binary_search_tree/test/multiset_test.rb
@@ -6,4 +6,37 @@ class MultiSetTest < Minitest::Test
   def test_multi_set
     assert MultiSet 
   end
+
+  def test_count
+    multiset = MultiSet.new([1, 2, 2, 3, 3, 3])
+    assert 1, multiset.count(1)
+    assert 2, multiset.count(2)
+    assert 3, multiset.count(3)
+  end
+
+  def test_max 
+    multiset = MultiSet.new([1, 2, 3, 3, 5, 5, 5, 6])
+    assert 6, multiset.max 
+  end
+
+  def test_min
+    multiset = MultiSet.new([1, 2, 3, 3, 5, 5, 5, 6])
+    assert 1, multiset.min
+  end
+
+  def test_insert 
+    multiset = MultiSet.new([1, 2, 2, 3, 3, 3])
+    multiset.insert(2)
+    assert 3, multiset.count(2)
+    multiset.insert(5)
+    assert 1, multiset.count(5)
+  end
+
+  def test_delete 
+    multiset = MultiSet.new([1, 2, 2, 3, 3, 3])
+    multiset.delete(3, 1)
+    assert 2, multiset.count(3)
+    multiset.delete(2, 10)
+    assert 0, multiset.count(2)
+  end
 end
