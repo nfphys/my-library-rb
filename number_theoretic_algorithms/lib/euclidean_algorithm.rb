438edb7f80e1f230da01b3ce966f060935f00050
nfphys <nfphys@gmail.com>
2022-10-11 23:07:54 +0900

    solve_bezoutを追加

--- number_theoretic_algorithms/lib/euclidean_algorithm.rb
+++ number_theoretic_algorithms/lib/euclidean_algorithm.rb
@@ -1,7 +1,27 @@
 module EuclideanAlgorithm 
+  # 整数 a, b の最大公約数を求める。
   def gcd(a, b) 
-    a, b = b, a if a < b 
-    return a if b === 0
+    a, b = b, a if a.abs < b.abs
+    return a.abs if b === 0
     gcd(b, a%b)
+  end
+
+  # 整数 a, b に対する Bezout's identity
+  #   ax + by = gcd(a, b)
+  # の解を一つ求める。
+  def solve_bezout(a, b) 
+    if a.abs < b.abs
+      y, x = solve_bezout(b, a)
+      return [x, y]
+    end 
+
+    if b === 0
+      return [a >= 0 ? 1 : -1, 0] 
+    end
+
+    r = a%b 
+    q = (a-r)/b
+    s, t = solve_bezout(b, r)
+    [t, s - q*t]
   end
 end
--- number_theoretic_algorithms/test/euclidean_algorithm_test.rb
+++ number_theoretic_algorithms/test/euclidean_algorithm_test.rb
@@ -8,5 +8,15 @@ class EuclideanAlgorithmTest < Minitest::Test
     assert_equal 1, gcd(2, 3)
     assert_equal 3, gcd(6, 9)
     assert_equal 2, gcd(32, 18)
+    assert_equal 1, gcd(3, -7)
+  end
+
+  def test_solve_bezout 
+    10.times do 
+      a = rand(-100..100)
+      b = rand(-100..100)
+      x, y = solve_bezout(a, b)
+      assert_equal gcd(a, b), a*x + b*y
+    end
   end
 end
